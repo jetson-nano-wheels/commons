b81ccf5c61564ef528b16ec1b403a25809ca1474
austinjp <austin.plunkett@gmail.com>
2021-08-24 17:35:25 +0100

    A few more items.
    Added alternative versions of gcc and g++ since one build only works with an older version.
    
    Added a couple of libboost-related deps.

--- init.sh
+++ init.sh
@@ -12,9 +12,11 @@ sudo apt-get install \
      build-essential make cmake \
      python3-dev python3.6-dev python3.7-dev \
      ocl-icd-opencl-dev \
-     libboost-all-dev \
+     libboost-all-dev libboost-python-dev libboost-thread-dev \
      libopenblas-base libopenmpi-dev \
      nvidia-jetpack cuda-toolkit-10-2 \
      cuda-documentation-10-2 cuda-gdb-10-2 cuda-libraries-10-2 cuda-libraries-dev-10-2 cuda-nvcc-10-2 cuda-toolkit-10-2 cuda-tools-10-2 \
      libcublas-dev libcublas10 \
+     gcc-7  g++-7  gfortran-7 \
+     gcc-9  g++-9  gfortran-9 \
      gcc-11 g++-11 gfortran-11
