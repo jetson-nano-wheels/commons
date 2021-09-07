2c2c4781d8a70fa873914bb15003150ea793c8e1
austinjp <austin.plunkett@gmail.com>
2021-09-07 15:38:41 +0100

    Added/reorganised more deps

--- init.sh
+++ init.sh
@@ -3,20 +3,40 @@
 # Install all (known) system requirements.
 # Note, this is not for any particular wheel, but for *all* of them.
 # You could take a more efficient approach and only install the
-# packages you actually need, if you so desire.
+# packages you actually need if you prefer.
 
-# TODO: Test before installing, so this script can be
+# TODO: Test each dep before installing, so this script can be
 # executed without thinking/waiting.
 
-sudo apt-get install \
-     build-essential make cmake \
-     python3-dev python3.6-dev python3.7-dev \
-     ocl-icd-opencl-dev \
-     libboost-all-dev libboost-python-dev libboost-thread-dev \
-     libopenblas-base libopenmpi-dev \
-     nvidia-jetpack cuda-toolkit-10-2 \
-     cuda-documentation-10-2 cuda-gdb-10-2 cuda-libraries-10-2 cuda-libraries-dev-10-2 cuda-nvcc-10-2 cuda-toolkit-10-2 cuda-tools-10-2 \
-     libcublas-dev libcublas10 \
+sudo apt-get install wget
+
+OS_VERSION=$(lsb_release -a 2>/dev/null | grep Release | awk '{print $2}' | sed -e 's/\.//g' )
+OS="ubuntu${OS_VERSION}"
+pushd /tmp/ 2>&1 > /dev/null \
+    && wget https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin \
+    && sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600 \
+    && popd 2>&1 > /dev/null
+
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/7fa2af80.pub \
+    && sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/ /"
+
+sudo add-apt-repository ppa:graphics-drivers/ppa
+
+sudo apt update
+
+sudo apt-get install -y \
+     build-essential make cmake software-properties-common \
+     cuda-documentation-10-2 cuda-gdb-10-2 cuda-libraries-10-2 cuda-libraries-dev-10-2 \
+     cuda-nvcc-10-2 cuda-toolkit-10-2 cuda-tools-10-2 \
      gcc-7  g++-7  gfortran-7 \
      gcc-9  g++-9  gfortran-9 \
-     gcc-11 g++-11 gfortran-11
+     gcc-11 g++-11 gfortran-11 \
+     libboost-all-dev libboost-python-dev libboost-thread-dev \
+     libcublas-dev libcublas10 \
+     libopenblas-base libopenblas-dev \
+     libopenmpi-dev \
+     nvidia-jetpack cuda-toolkit-10-2 \
+     ocl-icd-opencl-dev \
+     python3.6 python3.8 python-dev python3-dev python3.6-dev python3.8-dev \
+     python3-h5py libhdf5-dev \
+     python3-jsmin
